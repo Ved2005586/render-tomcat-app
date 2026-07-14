d5ba2c7465c9dbc3efe54521e515c985fedff5e9
Vedant Salunkhe <vedant2005@gmail.com>
2026-07-14 23:08:30 +0530

    Disable Tomcat shutdown port to clean up logs

--- start.sh
+++ start.sh
@@ -6,4 +6,8 @@ PORT="${PORT:-8080}"
 
 sed -i "s/port=\"8080\"/port=\"${PORT}\"/" /usr/local/tomcat/conf/server.xml
 
+# Disable the shutdown port so Render's health check pings don't spam
+# the logs with "Invalid shutdown command" warnings.
+sed -i 's/port="8005" shutdown="SHUTDOWN"/port="-1" shutdown="SHUTDOWN"/' /usr/local/tomcat/conf/server.xml
+
 exec /usr/local/tomcat/bin/catalina.sh run
